dbdd141d21589f366e94b44c6d6ef4eba1976137
Janire <janire.diaz@gmail.com>
2015-11-25 19:17:01 +0100

    new validations

--- Formulario/validacion.js
+++ Formulario/validacion.js
@@ -1,32 +1,34 @@
 function validacion() {
-    valor = $("#nombre").value;
+    
+
+    valor = $("#nombre").val();
     if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
         alert("El nombre introducido no es válido, introducelo de nuevo y vuelve a intentarlo");
         return false;
     }
+    alert("hola");
 
-    valor = document.getElementById("apellido").value;
+    valor = $("#apellido").val();
     if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
         alert("Los apellidos introducidos no son válidos, introducelos de nuevo y vuelve a intentarlo");
         return false;
     }
 
-    valor = document.getElementById("telefono").value;
-    if( isNaN(valor) ) {
+    valor = $("#telefono").value;
+    if( isNaN(valor) || valor == null || valor.length == 0 ) {
         alert("El telefono introducido no es valido");
         return false;
     }
 
 
-    valor = document.getElementById("correo").value;
+    valor = $("#correo").value;
     if (/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/.test(valor)){
         alert("La dirección de email es incorrecta");
         return false;
     }
 
-    alert("hola");
 
-    indice = document.getElementById("provincia").selectedIndex;
+    indice = $("#provincia").selectedIndex;
     if( indice == null || indice == 0 ) {
         return false;
     }
